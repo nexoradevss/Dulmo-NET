c711142c12ae8cc5a79af2e1bdb88432d4f573f7
Mohamad <mohamadyuonsos@gmail.com>
2026-06-04 22:33:17 +0300

    Merge branch 'main' of https://github.com/nexoradevss/Dulmo-NET # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- docs/sw.js
+++ docs/sw.js
@@ -1,10 +1,15 @@
-﻿const CACHE = 'dolmusnet-v1';
-const FILES = ['./passenger.html', './manifest.json'];
-
+const CACHE = 'dolmusnet-v2';
+const FILES = [
+  '/Dulmo-NET/passenger.html',
+  '/Dulmo-NET/manifest.json'
+];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
 });
-
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
